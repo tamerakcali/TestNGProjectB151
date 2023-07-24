9a621bfcb447bf9cae21aead015258e9771b20ab
tamerakcali <tamerakcali@gmail.com>
2023-07-24 21:04:34 +0200

    US001_V2

--- src/main/java/test/java/tests/US001/TC01_SearchBoxTest.java
+++ src/main/java/test/java/tests/US001/TC01_SearchBoxTest.java
@@ -1,4 +1,12 @@
 package tests.US001;
 
+import org.testng.annotations.Test;
+import utilities.Driver;
+
 public class TC01_SearchBoxTest {
+    @Test
+    public void test01() {
+
+        Driver.closeDriver();
+    }
 }
